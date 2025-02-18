a23e76e579d43d4e34b6c2ece08c5ef7315fd05a
Takeshi Miura <101356765+Takeshi-mi@users.noreply.github.com>
2025-02-18 00:33:21 -0300

    Initial Overleaf Import

---
+++ README.md
@@ -0,0 +1,5 @@
+# Modelo de TCC para o TADS do IFG Formosa
+
+Par clonar o repositório, digite: `git clone https://github.com/Takeshi-mi/Latex-TCC-Takeshi-Arqueologia-em-Pixels.git`
+
+Para compilar digite `./compilar.sh`
---
+++ compilar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -f *.aux *.mtc* *.out *.idx *.bcf *.log *.lof *~ *.ptc *.toc *.run.xml *.maf *.blg
+
+pdflatex main -interaction=nonstopmode
+
+bibtex main
+
+makeglossaries main
+
+pdflatex main -interaction=nonstopmode
+
+pdflatex main -interaction=nonstopmode
+
+rm -f *.aux *.mtc* *.out *.idx *.bcf *.log *.lof *~ *.ptc *.toc *.run.xml *.maf *.blg
